e386781753985b1a6fcf5661f9768e814f6d7263
a19nadav <a15nadav@gmail.com>
2024-08-11 00:54:29 +0300

    deploy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -130,7 +128,7 @@ tasks {
         dependsOn("shadowJar")
     }
 
-    withType<ShadowJar> {
+    withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
         manifest {
             attributes(mapOf("Main-Class" to "app.ServerMain"))
         }
