76027a97eadb47af6b2330f53fd1c51a744e4ea2
a19nadav <a15nadav@gmail.com>
2024-08-12 01:02:42 +0300

    check

--- server/src/main/java/com/example/server/Server.kt
+++ server/src/main/java/com/example/server/Server.kt
@@ -200,7 +200,7 @@ object Server {
             }
         }
     }
-
+//
     private fun Application.configureDatabase() {
         val dbUrl = "postgres://u3muoju0j6oajo:pfcfb2100486e690377ab4266f1c5a4af296db4180e09961058a77a34745c000c@cat670aihdrkt1.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d5amu549gim58e"
         val dbUri = URI(dbUrl)
